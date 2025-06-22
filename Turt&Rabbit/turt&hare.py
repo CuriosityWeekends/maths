b5e986beac767e5c0ec07e2ec939e54870a490b1
Ishan <66067552+ishan-jemshid@users.noreply.github.com>
2025-06-22 14:59:23 +0530

    Add files via upload

--- Turt&Rabbit/turt&hare.py
+++ Turt&Rabbit/turt&hare.py
@@ -4,11 +4,15 @@ import matplotlib.pyplot as plt
 turt = 10 
 hare = 0
 
-hare_speed = 2
+hare_speed = 1.3
 turt_speed = 1
 
 dist_hare = float(0)
 dist_turt = float(0)
+
+x_vals = []
+hare_vals = []
+turt_vals = []
 
 t = float(0)
 
@@ -17,28 +21,34 @@ x = range(0, stx)
 yturt = 0
 yhare = 0
 
-while True:
+plt.ion()
+
+while True: 
 
     stx += 1
-    x = range(0, stx)
+
 
     dist_hare = turt - hare
     hare = hare + dist_hare
-    #print(dist_hare)
+
     t = dist_hare/hare_speed
     dist_turt = t * turt_speed
     turt = turt + dist_turt
 
+
+    x_vals.append(t)
+    hare_vals.append(hare)
+    turt_vals.append(turt)
+
     plt.clf()
-    plt.plot(x, hare,"red" ,label='hare')
-    plt.plot(x, turt,"blue" ,label='turt')
+    plt.plot(x_vals, hare_vals,color="red" ,label='hare')
+    plt.plot(x_vals, turt_vals,color="blue" ,label='turt')
     plt.xlabel('time')
     plt.ylabel('distance')
     plt.title('race between the Turt & the Hare')
 
     plt.legend()
     plt.grid(True)
-    plt.draw()
     plt.pause(0.1)
 
 
