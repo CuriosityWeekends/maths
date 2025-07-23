e8598f4db465397d75693ecd982a49a3b5df81a9
Ishan <66067552+ishan-jemshid@users.noreply.github.com>
2025-07-23 18:43:30 +0530

    added closeness measure

--- wifi_localizer/wifi_dist.py
+++ wifi_localizer/wifi_dist.py
@@ -1,8 +1,13 @@
 import subprocess
 import time
 import pandas as pd
+import numpy as np
 
 a = []
+
+def scan_wifi():
+    subprocess.run(["nmcli", "dev", "wifi", "rescan"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+
 
 def list_nearby_wifi():
 
@@ -19,8 +24,23 @@ def list_nearby_wifi():
         signal = int(parts[-1])
         WifiList.append((ssid, signal))
     
-    WifiList = pd.DataFrame(WifiList)
-    return WifiList
+    wifiList = pd.DataFrame(WifiList)
+    wifiList.rename(columns={0:"ssid", 1:"signal_strength"}, inplace=True)
+
+    # remove duplicate SSIDs due to 2.4 and 5 GHz by combining duplicates and taking the mean signal stregth
+    wifiList = wifiList.groupby('ssid', as_index=False)['signal_strength'].mean()
+
+    return wifiList
+
+def Closeness(wWhereYouAre, wKnown):
+    # calculates a closeness measure with pythogras approach
+    wCloseness = wKnown.merge(wWhereYouAre, on="ssid", how="left")
+    wCloseness = wCloseness.fillna(0)
+    closeness = wCloseness["signal_strength_y"] - wCloseness["signal_strength_x"]
+    closeness = closeness ** 2
+    closeness = closeness.sum() / len(closeness)
+    closeness = np.sqrt(closeness)
+    return closeness
 
 while True:
     a = list_nearby_wifi()
