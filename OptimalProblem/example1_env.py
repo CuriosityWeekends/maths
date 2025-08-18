030b5cc468b050d3e8f84abb94fa189eaa5e65f8
Hadin AH <libapayapura+git@gmail.com>
2025-08-18 23:59:25 +0530

    Created the environment seperately

---
+++ OptimalProblem/example1_env.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+routers = np.array([
+    [0, 10],
+    [20, 0],
+    [25, 0],
+]) #Since we have only 3 routers, we can use a fixed array (for simplicity)
+
+def find_distances(point, routers=routers):
+    """
+    Distances from point to each router.
+    """
+    return np.linalg.norm(routers - point)
+
+def distance_between_routers(distances):
+    n = len(distances)
+    results = {}
+    for i in range(n):
+        for j in range(i+1, n):
+            dmin = abs(distances[i] - distances[j])
+            dmax = distances[i] + distances[j]
+            results[(i, j)] = (dmin, dmax)
+    return results
