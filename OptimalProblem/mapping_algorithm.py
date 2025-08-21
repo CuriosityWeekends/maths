d88c2d65f4ae9bc0bff99aad5b16f1d3371f0064
Hadin AH <libapayapura+git@gmail.com>
2025-08-21 23:56:46 +0530

    Best possible number from heatmap
    Made a function To find the best heatspot in the heatmap is... (brightest spot) which should be the most probable number for the range

--- OptimalProblem/mapping_algorithm.py
+++ OptimalProblem/mapping_algorithm.py
@@ -27,6 +27,19 @@ def compute_coverage(all_distances, bins=500):
 
     return results
 
+def get_best_distances(coverage_results):
+    """
+    This should find the "brightest" spot in the coverage heatmap.
+    """
+    best_distances = {}
+
+    for pair, (x_vals, coverage) in coverage_results.items():
+        best_index = np.argmax(coverage)     # index of maximum coverage
+        best_distance = x_vals[best_index]   # distance at that index
+        best_distances[pair] = best_distance
+
+    return best_distances
+
 
 def plot_heatmap(coverage_dict):
     pairs = list(coverage_dict.keys())
@@ -54,7 +67,7 @@ if __name__ == "__main__":
     all_distances = []
     best_ranges = None
 
-    for _ in range(100):  # Iteration count
+    for _ in range(10000):  # Iteration count
         x = random.randint(-25, 25)
         y = random.randint(-25, 25)
         point = np.array([x, y])
@@ -64,7 +77,7 @@ if __name__ == "__main__":
 
     print("Best ranges found:")
     for pair, (dmin, dmax) in best_ranges.items():
-        print(f"Routers {pair}: {dmin:.2f} <> {dmax:.2f}")
+        print(f"Routers {pair}: {dmin} <> {dmax}")
 
-    coverage = compute_coverage(all_distances, bins=5000)
+    coverage = compute_coverage(all_distances, bins=50000)
     plot_heatmap(coverage)
